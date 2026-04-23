e852fdb2a3334d0b365aef4e520848a5664dd08d
SURAJ DATTATRAY GAVADE <surajgavadecloudblitz@gmail.com>
2026-04-23 19:33:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ Total: ₹{total}
 col1, col2 = st.columns([1,5])
 
 with col1:
-    st.image("logo.jpg", width=70)
+    st.image("logo.jpg", width=300)
 
 with col2:
     st.markdown("### 📚 राजहंस पुस्तक पेठ")
